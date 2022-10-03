0393e64f01d223ca1b375698f0bbe9d5aedefe85
Alice Lipski <alipski@pop-os.localdomain>
2022-10-03 15:56:55 -0300

    bug fix on database.js

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ import customersRouter from './routers/customersRouter.js';
 import rentalsRouter from './routers/rentalsRouter.js';
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(categoriesRouter);
 app.use(gamesRouter);
